84ae3d72efb444bac5b8ebbd1138f9eda7e79ad8
Corbin-Petersen <corbincpetersen@gmail.com>
2025-05-12 15:21:41 -0500

    finished all steps in the practice instructions

---
+++ todo-app/src/components/ToDoList.jsx
@@ -0,0 +1,12 @@
+const ToDoList = () => {
+    let tasks = ["Walk the dog", "Practice coding", "Bake a cake"]
+    return (
+        <ul>
+            <li>Walk the dog</li>
+            <li>Practice coding</li>
+            <li>Bake a cake</li>
+        </ul>
+    )
+}
+
+export default ToDoList
